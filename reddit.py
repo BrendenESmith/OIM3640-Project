fe29e9d70c476814b875efd47a939869acd08aa6
jleeeeee <77594057+jleeeeee@users.noreply.github.com>
2021-11-30 17:17:08 -0500

    Update reddit.py

--- reddit.py
+++ reddit.py
@@ -1,12 +1,18 @@
 import praw
+from textblob import TextBlob
 from personal import client_id, client_secret, password
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 reddit = praw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      username="dingdingding3",
                      password=password,
                      user_agent="user_agent")
-for submission in reddit.subreddit("learnpython").hot(limit=10):
-    print(submission.title)
+
+community = reddit.subreddit("learnpython").hot(limit=10)
+for submission in community:
+    print(f'{submission.title} by:{submission.author}      \ntotal comments: {submission.num_comments}\nurl: {submission.url}\n')
 
 # sub = 'learnpython'
 # submissions = reddit.subreddit(sub).top('day', limit=5)
